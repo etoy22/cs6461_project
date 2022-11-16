48ca45991e6d8fe09ad2f857bc83af7f0e598d70
etoy22 <ethanleider@gmail.com>
2022-11-16 14:54:38 -0500

    Create program2.java

---
+++ program2.java
@@ -0,0 +1,23 @@
+//Returns 0 for error 
+//Returns 1 if completed
+public class program2 {
+    public int isWordinParagraphs(String paragraph,String word){
+        String[] parts = paragraph.toLowerCase().split("\\.",0);
+        boolean found = false;
+        if(parts.length!=6){
+            return 0;
+        }
+        for(int i = 0;i<6 &&!found;i++){
+            parts[i].replace(".","");
+            String[] wNumber = parts[i].split(" ",0);
+            for(int j = 0;j<wNumber.length&&!found;j++){
+                if(wNumber[j].equals(word)){
+                    System.out.println("Word: "+ wNumber[j]);
+                    System.out.println("Sentence Number: "+ (i+1));
+                    System.out.println("Word Number in the Sentence: "+ j);
+                }
+            }
+        }
+        return 1;
+    }
+}
