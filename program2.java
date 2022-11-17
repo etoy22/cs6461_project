2392ec9607344b5b3b687bfada1bf5cbf3b81fa4
etoy22 <ethanleider@gmail.com>
2022-11-16 21:29:50 -0500

    Update program2.java

--- program2.java
+++ program2.java
@@ -10,11 +10,15 @@ public class program2 {
         for(int i = 0;i<6 &&!found;i++){
             parts[i].replace(".","");
             String[] wNumber = parts[i].split(" ",0);
+            int add = 0;
+            if(!wNumber[0].equals("")){
+                add = 1;
+            }
             for(int j = 0;j<wNumber.length&&!found;j++){
                 if(wNumber[j].equals(word)){
                     System.out.println("Word: "+ wNumber[j]);
                     System.out.println("Sentence Number: "+ (i+1));
-                    System.out.println("Word Number in the Sentence: "+ j);
+                    System.out.println("Word Number in the Sentence: "+ (j+add));
                 }
             }
         }
